acf56bc7f3ba3d8a12fc7b6d9d6d7012cae72706
Lethabo Mosoathupa <99099883+123lethabomosoathupa@users.noreply.github.com>
2025-07-09 09:22:45 +0200

    Add files via upload

---
+++ JavaScript For Kids/source-code/Chapter 2/Homework/TryOut(p23)/tryout_p23.js
@@ -0,0 +1,12 @@
+//Question 1
+
+var balloons = 100; //we are declaring the variable "balloons" and assigning it with the value of 100
+balloons *= 2;      //we are multiplying the value of balloons by 2
+200                 //This is the result that is given from that operation
+
+//Question 2
+
+var balloons = 100; //we are declaring the variable "balloons" and assigning it with the value of 100
+balloons /= 4;      //we are dividing the value of the balloons by 4
+25                  //This is the result of the given operation
+
---
+++ JavaScript For Kids/source-code/Chapter 2/Homework/Tryout(p37)/tryout_p37.js
@@ -0,0 +1,12 @@
+//Declaration of variables
+
+var age = 12;                                           //we declaring the variable 'age' and assigning it with the integer 12
+var accompanied = true;                                 //we declaring the variable 'accompanied' and assigning it as true
+var age_restriction = 13;                               //we are declaring the variable 'age_restriction' and assigning it with the integer 13
+var parental_vision = true;                             //we are declaring the variables 'parental_vision' and assigning it as true
+
+//Now we will be performing operations
+
+var age_acceptance = age > age_restriction;             //This will result in false
+
+var parental_guidance = accompanied && parental_vision; //This will result 

0cc39ac2114963ca677d63b33d7fff548b9766e9
Lethabo Mosoathupa <99099883+123lethabomosoathupa@users.noreply.github.com>
2025-07-11 14:42:22 +0200

    Add files via upload

---
+++ JavaScript For Kids/source-code/Chapter 6/tryout(pg96)/tryout96.js
@@ -0,0 +1,20 @@
+console.log("-----Part A-----")
+
+var name = "Lethabo";
+if (name === "Bob") {
+ console.log("Hello me!!!");
+} else {
+    console.log("Hello stranger...")
+}
+
+console.log("-----Part B-----")
+
+var mom = "Maria";
+var father = "Bob";
+if (mom){
+    console.log("Hello " +mom)
+} else if (father){
+    console.log("Hello " + bob)
+} else{
+    console.log("Hello Stranger")
+}

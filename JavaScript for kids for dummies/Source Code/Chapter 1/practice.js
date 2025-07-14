451dd22638ea5237d2ce1510a32596d92442184b
Lethabo Mosoathupa <99099883+123lethabomosoathupa@users.noreply.github.com>
2025-07-14 11:37:37 +0200

    Add files via upload

---
+++ JavaScript for kids for dummies/Source Code/Chapter 1/practice.js
@@ -0,0 +1,11 @@
+// Subtract 37 from 2000 and log the result
+console.log(2000 - 37);
+
+// Multiply 30 by 27 and log the result
+console.log(30 * 27);
+
+// Divide 120 by 20 and log the result
+console.log(120 / 20);
+
+// Concatenate strings and log the message
+console.log("Your name" + " " + "is learning JavaScript!");

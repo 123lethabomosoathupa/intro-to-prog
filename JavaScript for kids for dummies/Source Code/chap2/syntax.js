d8b5b6eb2ace6e51c6ea438a363288778e66096b
Lethabo Mosoathupa <99099883+123lethabomosoathupa@users.noreply.github.com>
2025-07-14 12:14:53 +0200

    Add files via upload

---
+++ JavaScript for kids for dummies/Source Code/chap2/syntax.js
@@ -0,0 +1,27 @@
+// Show an alert popup with the message "Coding is fun!"
+alert("Magestic");
+
+// Loop 10 times and write "Coding is fun!" to the document each time
+for (var i = 0; i < 10; i++) {
+  document.write("Coding is fun!<br>");
+}
+
+// Loop 5 times and write "Joe said, Hi!" to the document each time
+for (var i = 0; i < 5; i++) {
+  document.write("Joe said, Hi!<br>");
+}
+
+for (var i = 0; i <7; i++){
+    document.write("I am magestic<br>")
+}
+
+/* 
+-----------multi comment line----------
+
+Write some text directly to the HTML output by using document.write().
+document.write() still works perfectly in all major browsers for now, but it's deprecated, meaning it's discouraged and may be removed in future versions.
+
+ " is an escape character.It tells JavaScript to treat the double quote as a character, not the end of the string.
+ 
+*/
+
